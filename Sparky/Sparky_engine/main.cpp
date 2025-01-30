75290c15b884ab92f9a3ee66c1a8d78c00b3bb24
Kacp-rn <kacp.r.nalepa@gmail.com>
2025-01-30 09:37:37 +0100

    Add files via upload

--- Sparky/Sparky_engine/main.cpp
+++ Sparky/Sparky_engine/main.cpp
@@ -10,6 +10,14 @@
 
 #include"src/graphics/renderer2d.h"
 #include"src/graphics/simple2drenderer.h"
+#include"src/graphics/batchrenderer2d.h"
+
+#include"src/graphics/static_sprite.h"
+#include"src/graphics/sprite.h"
+
+#include<time.h>
+
+#define BATCH_RENDERER 1
 
 int main()
 {
@@ -28,17 +36,41 @@ int main()
 	shader.enable();
 
 	shader.setUniformMat4("pr_matrix", ortho);
-	shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
+//	shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
+	
+	std::vector<Renderable2D*> sprites;
 
-	   Renderable2D sprite(maths::vec3(5, 5, 0), maths::vec2(4,4), maths::vec4(0.1, 0.5, 0.7, 1), shader);
-	   Renderable2D sprite2(maths::vec3(0, 5, 0), maths::vec2(4, 4), maths::vec4(0.3, 0.6, 0.9, 1), shader);
-	   Renderable2D sprite3(maths::vec3(0, 0, 0), maths::vec2(4, 4), maths::vec4(0.4, 0.2, 0.8, 1), shader);
-	   Renderable2D sprite4(maths::vec3(5, 0, 0), maths::vec2(4, 4), maths::vec4(0.7, 0.3, 0.5, 1), shader);
-	   Renderable2D sprite5(maths::vec3(10, 5, 0), maths::vec2(4, 4), maths::vec4(0.9, 0.1, 0.4, 1), shader);
-	   Renderable2D sprite6(maths::vec3(10, 0, 0), maths::vec2(4, 4), maths::vec4(0.6, 0.8, 0.2, 1), shader);
+	srand(time(NULL));
 
+	for (float y = 0; y < 9.0f; y+= 0.05)
+	{
+		for (float x = 0; x < 16.0f; x += 0.05)
+		{
+			sprites.push_back(new
+#if BATCH_RENDERER
+				Sprite
+#else
+				StaticSprite
+#endif	
+				(x, y, 0.04f, 0.04f, maths::vec4(rand() % 1000 / 1000.0f, rand() % 1000 / 1000.0f, rand() % 1000 / 1000.0f, 1)
+#if !BATCH_RENDERER
+				,  shader
+#endif
+				));
+		}
+	}
+
+#if BATCH_RENDERER
+	Sprite sprite(5, 5, 4, 4, maths::vec4(0.1, 0.5, 0.7, 1));
+	Sprite sprite2(0, 5, 4, 4, maths::vec4(1.0, 0.1, 0.0, 1));
+	BatchRenderer2D renderer;
+
+	
+#else
+	StaticSprite sprite(5, 5, 4, 4, maths::vec4(0.1, 0.5, 0.7, 1), shader);
+	StaticSprite sprite2(0, 5, 4, 4, maths::vec4(1.0, 0.1, 0.0, 1), shader);
 	Simple2DRenderer renderer;
-
+#endif
 
 	shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
 	//shader.setUniform4f("colour", vec4(0.2f, 0.3f, 0.8f, 1.0f));
@@ -50,14 +82,16 @@ int main()
 			double x, y;
 			window.getMousePosition(x, y);
 			shader.setUniform2f("light_pos", vec2((float)(x * 16.0f / 960.0f), (float)(9.0f - y * 9.0f / 540.0f)));
-
-			renderer.submit(&sprite);
-			renderer.submit(&sprite2);
-			renderer.submit(&sprite3);
-			renderer.submit(&sprite4);
-			renderer.submit(&sprite5);
-			renderer.submit(&sprite6);
-
+#if BATCH_RENDERER
+			renderer.begin();
+#endif
+			for(int i = 0; i<sprites.size(); i++)
+			{ 
+			renderer.submit(sprites[i]);
+			}
+#if BATCH_RENDERER
+			renderer.end();
+#endif
 			renderer.flush();
 
 			
